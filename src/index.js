1c8757629781bae8186cc85f70c9b4206f1342a1
SagaMS <91miablond@gmail.com>
2024-10-27 17:08:29 +0700

    feat:solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig){
+  if(str.length % 2 !==0){
+    return false
+  }else{
+    function removePaar(str, bracets){
+ 
+      const paar = bracets[0]+ bracets[1]
+ 
+      for (let index = 0; index <= str.length/2; index++) {
+        str = str.replace(paar, '');
+      }
+      return str;
+      }
+
+
+    let count = str.length/2;
+    while(count>=0){
+      for (let index = 0; index < bracketsConfig.length; index++) {
+        const brack = bracketsConfig[index];
+        str = removePaar(str, brack)
+        if(str.length === 0){
+          return true
+        }
+      }
+      count --;
+    }
+      return false;
+
+  }
 }
+
+
+
+
+
+
